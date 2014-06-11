439d3dd5dd7ba9e6197b75f7c0a10fe7f103c194
Akira Sudoh <asudoh@gmail.com>
2014-06-11 21:35:25 +0900

    Make deduper plugin ignore undocumented doclet.

--- custom-plugins/dedupe.js
+++ custom-plugins/dedupe.js
@@ -3,7 +3,7 @@
 exports.handlers = {
     processingComplete: function(e) {
         for (var table = {}, doclets = e.doclets, i = doclets.length - 1; i >= 0; --i) {
-            if (doclets[i].memberof) {
+            if (doclets[i].memberof && !doclets[i].undocumented) {
                 if (table[doclets[i].longname]) {
                     doclets.splice(i, 1);
                 } else {
